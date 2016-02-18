004c498343654b49a7633b6d9c6bb3a17bde487d
yangyankai <yangyankai_mr@outlook.com>
2016-02-18 16:37:12 +0800

    Update Event.java

--- app/src/main/java/com/sf/manager/eventbus2/Event.java
+++ app/src/main/java/com/sf/manager/eventbus2/Event.java
@@ -3,22 +3,23 @@ package com.sf.manager.eventbus2;
 
 import java.util.List;
 
-public class Event
-{
-    /** 列表加载事件 */
-    public static class ItemListEvent
-    {
-        private List<Item> items;
+public class Event {
+    /**
+     * 列表加载事件
+     */
+    public static class ItemListEvent {
+        private List<String> items;
 
-        public ItemListEvent(List<Item> items)
-        {
-            this.items = items;
+        public ItemListEvent(int n) {
+            items = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                items.add("第" + (i + 1) + "个");
+            }
         }
 
-        public List<Item> getItems()
-        {
+        public List<String> getItems() {
             return items;
         }
     }
-
 }
+
