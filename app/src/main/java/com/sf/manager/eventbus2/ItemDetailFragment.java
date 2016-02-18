e974430760977b236c1e4f4f631931366ee380c4
yangyankai <yangyankai_mr@outlook.com>
2016-02-18 16:38:20 +0800

    Update ItemDetailFragment.java

--- app/src/main/java/com/sf/manager/eventbus2/ItemDetailFragment.java
+++ app/src/main/java/com/sf/manager/eventbus2/ItemDetailFragment.java
@@ -9,41 +9,36 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import de.greenrobot.event.EventBus;
 
-public class ItemDetailFragment extends Fragment
-{
+public class ItemDetailFragment extends Fragment {
 
     private TextView tvDetail;
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // register 订阅事件
         EventBus.getDefault().register(this);
     }
 
     @Override
-    public void onDestroy()
-    {
+    public void onDestroy() {
         super.onDestroy();
         // Unregister 取消订阅
         EventBus.getDefault().unregister(this);
     }
 
-    /** List点击时会发送些事件，接收到事件后更新详情 */
-    public void onEventMainThread(Item item)
-    {
-        Log.e("eve", "Item");
-        if (item != null)
-            tvDetail.setText(item.content);
+    /**
+     * List点击时会发送些事件，接收到事件后更新详情
+     */
+    public void onEventMainThread(String str) {
+        Log.e("eve", "Item  do in other class");
+        if (str != null)
+            tvDetail.setText(str);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
-    {
-        View rootView = inflater.inflate(R.layout.fragment_item_detail,
-                container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragmemt_item_detail, container, false);
         tvDetail = (TextView) rootView.findViewById(R.id.item_detail);
         return rootView;
     }
