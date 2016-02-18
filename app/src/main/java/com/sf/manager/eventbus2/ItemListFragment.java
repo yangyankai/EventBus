dd45080fa77ab085304575c80a9360fca3b7cd5b
yangyankai <yangyankai_mr@outlook.com>
2016-02-18 16:39:28 +0800

    Update ItemListFragment.java

--- app/src/main/java/com/sf/manager/eventbus2/ItemListFragment.java
+++ app/src/main/java/com/sf/manager/eventbus2/ItemListFragment.java
@@ -11,59 +11,39 @@ import android.widget.ListView;
 
 import de.greenrobot.event.EventBus;
 
-public class ItemListFragment extends ListFragment
-{
+
+public class ItemListFragment extends ListFragment {
+
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         // Register 订阅事件
         EventBus.getDefault().register(this);
     }
 
     @Override
-    public void onDestroy()
-    {
+    public void onDestroy() {
         super.onDestroy();
         // Unregister 取消订阅
         EventBus.getDefault().unregister(this);
     }
 
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState)
-    {
+    public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        // 开启线程加载列表
-        new Thread()
-        {
-            public void run()
-            {
-                try
-                {
-                    Thread.sleep(2000); // 模拟延时
-                    // 发布事件，在后台线程发的事件。 在下面绑定适配器处相应
-                    EventBus.getDefault().post(new Event.ItemListEvent(Item.ITEMS));
-                } catch (InterruptedException e)
-                {
-                    e.printStackTrace();
-                }
-            };
-        }.start();
+        Event.ItemListEvent event = new Event.ItemListEvent(7);
+        EventBus.getDefault().post(event);
     }
 
-    public void onEventMainThread(Event.ItemListEvent event)
-    {
-        Log.e("eve", "new Event.ItemListEvent(Item.ITEMS)");
-        setListAdapter(new ArrayAdapter<Item>(getActivity(),
-                android.R.layout.simple_list_item_activated_1,
-                android.R.id.text1, event.getItems()));
+    public void onEventMainThread(Event.ItemListEvent event) {
+        Log.e("eve", "  do in this class ");
+        setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_activated_1, android.R.id.text1, event.getItems()));
     }
 
     @Override
-    public void onListItemClick(ListView listView, View view, int position,
-                                long id)
-    {
+    public void onListItemClick(ListView listView, View view, int position, long id) {
         super.onListItemClick(listView, view, position, id);
         EventBus.getDefault().post(getListView().getItemAtPosition(position));
     }
